0c71b27a1d498f3d272345973584efdede23dd68
Huseyin <huseyineskan01@gmail.com>
2025-04-23 21:32:18 +0200

    First commit

---
+++ Contoso Pets Application/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Contoso_Pets_Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWelcome to Pets App\n");
+            bool condition = true;
+            while (condition)
+            {
+                Console.WriteLine("Please select a selection:\n1.Add a new pet");
+                string menu = Console.ReadLine();
+                Console.WriteLine("menu");
+                Console.WriteLine();
+
+                switch (menu)
+                {
+                    case "exit":
+                        condition = false;
+                        break;
+                    case "1":
+                        Console.WriteLine("You selected 1");
+                        Console.WriteLine(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("No choice!");
+                        break;
+                }
+            }
+        }
+    }
+}
